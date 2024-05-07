b8e9d80814708817611e67dce9990281e756eee1
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-05-07 23:13:47 +0530

    rust_overflow

--- practice1/src/main.rs
+++ practice1/src/main.rs
@@ -1,6 +1,16 @@
+use std::collections::btree_map::Keys;
+
 fn main() {
     println!("Hello, world!");
     let x; i32 =5 ;
     let y: u32 = 1000 ;
     let z: f32 = 1000.001;
+
+    let mut k: i32 = 10;
+
+    for i; i32 in 0..1000 {
+        k= k+ 100;
+    }
+
+    print!("x = {} ", k )
 }
