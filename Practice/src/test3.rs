a22e232be0a90f0b254884a0d81de726cb85d98d
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-09-08 23:19:00 +0530

    Clean

--- Practice/src/test3.rs
+++ Practice/src/test3.rs
@@ -10,15 +10,19 @@ fn main() {
 
     // a signed integer 
 
-    let c : i32 = 5;
+    let _c : i8 = 5;
+    let mx : i8 = 127; 
+    let mn :i8 = -128;
 
     // unsigned integer
 
-    let d : u32 = 10;
+    let _d : u32 = 10;
+
 
     //operator 
 
-    let d = c + d;
+    let _ad = mn + mx;
+
 
     
 
