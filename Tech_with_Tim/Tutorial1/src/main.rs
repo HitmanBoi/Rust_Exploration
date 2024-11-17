4e5cc37235adfa8949bc0fad799e4b1f6103ab10
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-11-17 20:03:34 +0530

    Tech_with_Tim_Constants

--- Tech_with_Tim/Tutorial1/src/main.rs
+++ Tech_with_Tim/Tutorial1/src/main.rs
@@ -1,12 +1,4 @@
 fn main() {
-    let mut x = 5;
-    println!("x is: {}", x);
-
-    {
-        x = "Hello";
-        println!("x is: {}", x);
-    }
-
-    x = x+1;
-    println!("x is: {}", x);
+    const SECONDS_IN_MINUTE : u32 = 60 ;
+    println!("{}",SECONDS_IN_MINUTE);
 }
