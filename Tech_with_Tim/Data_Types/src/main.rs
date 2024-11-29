098a38de6b533cd59e6d1650b8d54e6cbc6f201b
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-11-29 20:03:29 +0530

    i32 range

--- Tech_with_Tim/Data_Types/src/main.rs
+++ Tech_with_Tim/Data_Types/src/main.rs
@@ -6,10 +6,17 @@ fn main() {
     //Signed Intergers (i8 i16 i32 i64 i128)
 
     let num1:i8 = i8::MIN ; //-128 to 127
-    println!("The i8 type of signed integers ranges from {} to {}",num1,i8::MAX);
+    println!("The i8 type of signed integers ranges from {} to {}.",num1,i8::MAX);
 
     let num2:i16 = i16::MAX; //-32768 to 32767
-    println!("The i8 type of signed integers ranges from {} to {}",i16::MIN,num2);
+    println!("The i16 type of signed integers ranges from {} to {}.",i16::MIN,num2);
+
+    let mut num3:i32 = i32::MIN ; //-2147483648 to 2147483647
+    print!("The i32 type of signed intergers ranges from {}",num3);
+    num3 = i32::MAX;
+    println!(" to {}.",num3);
+
+    
 
 
 
