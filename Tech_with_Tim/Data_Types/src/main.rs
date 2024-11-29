4fad7b8b5d74beff88df18c37c6934bf828bc8da
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-11-29 20:02:34 +0530

    i8 and i16 ranges

--- Tech_with_Tim/Data_Types/src/main.rs
+++ Tech_with_Tim/Data_Types/src/main.rs
@@ -8,5 +8,10 @@ fn main() {
     let num1:i8 = i8::MIN ; //-128 to 127
     println!("The i8 type of signed integers ranges from {} to {}",num1,i8::MAX);
 
+    let num2:i16 = i16::MAX; //-32768 to 32767
+    println!("The i8 type of signed integers ranges from {} to {}",i16::MIN,num2);
+
+
+
 
 }
