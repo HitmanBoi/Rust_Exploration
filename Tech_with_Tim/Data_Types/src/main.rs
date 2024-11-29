2e8a69280a582f48449d0e98c982761e9071f611
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-11-29 20:01:03 +0530

    Data Types

--- Tech_with_Tim/Data_Types/src/main.rs
+++ Tech_with_Tim/Data_Types/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    //explicit_decesion
-    let x:i8 = 2;
+    //Primitibve Data Types
 
-    
+    //Numbers 
+
+    //Signed Intergers (i8 i16 i32 i64 i128)
+
+    let num1:i8 = i8::MIN ; //-128 to 127
+    println!("The i8 type of signed integers ranges from {} to {}",num1,i8::MAX);
+
+
 }
