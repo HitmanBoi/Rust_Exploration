af702f9d8ed5f5be6b55098e069e75c2399e7d03
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-11-29 20:04:58 +0530

    i32 range implicit

--- Tech_with_Tim/Data_Types/src/main.rs
+++ Tech_with_Tim/Data_Types/src/main.rs
@@ -11,12 +11,12 @@ fn main() {
     let num2:i16 = i16::MAX; //-32768 to 32767
     println!("The i16 type of signed integers ranges from {} to {}.",i16::MIN,num2);
 
-    let mut num3:i32 = i32::MIN ; //-2147483648 to 2147483647
-    print!("The i32 type of signed intergers ranges from {}",num3);
+    let mut num3 = 24 ; //-2147483648 to 2147483647 , i32 is default implicit type for integers
+    print!("The i32 type of signed intergers ranges from {}",i32::MIN);
     num3 = i32::MAX;
     println!(" to {}.",num3);
 
-    
+    //let num4:i64 =
 
 
 
