52f5e8f3d175aa9458b76326f8f2e93f190874f3
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-12-09 20:31:41 +0530

    importing prelude

---
+++ Tech_with_Tim/Input_STD/target/debug/Input_STD.d
@@ -0,0 +1,1 @@
+D:\Rust_Exploration\Tech_with_Tim\Input_STD\target\debug\Input_STD.exe: D:\Rust_Exploration\Tech_with_Tim\Input_STD\src\main.rs
---
+++ Tech_with_Tim/Input_STD/target/debug/deps/Input_STD.d
@@ -0,0 +1,5 @@
+D:\Rust_Exploration\Tech_with_Tim\Input_STD\target\debug\deps\Input_STD.exe: src\main.rs
+
+D:\Rust_Exploration\Tech_with_Tim\Input_STD\target\debug\deps\Input_STD.d: src\main.rs
+
+src\main.rs:
