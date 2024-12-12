eaa030aa51a3cdf7a68b58c32ffe148607f6e597
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-12-12 20:14:56 +0530

    Comment Referances

--- Tech_with_Tim/Arithematics_TwT/src/main.rs
+++ Tech_with_Tim/Arithematics_TwT/src/main.rs
@@ -3,19 +3,19 @@ fn main() {
     let y = 10.0;
 
     let add1 = x + y ;
-    println!("{} + {} = {}",x,y,add1);
+    println!("{} + {} = {}",x,y,add1); //Addition
     
     let sub1 = x - y ;
-    println!("{} - {} = {}",x,y,sub1);
+    println!("{} - {} = {}",x,y,sub1); // Subtraction 
 
     let mul1 = x * y ;
-    println!("{} x {} = {}",x,y,mul1);
+    println!("{} x {} = {}",x,y,mul1); // Multiplication
 
     let div_que = x / y ;
-    println!("{} / {} = {}",x,y,div_que);
+    println!("{} / {} = {}",x,y,div_que); // Division for quotient
 
     let div_rem = x % y ;
-    println!("{} % {} = {}",x,y,div_rem);
+    println!("{} % {} = {}",x,y,div_rem); // Division for remainder
     
     
     
