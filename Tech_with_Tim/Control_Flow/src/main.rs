162c93c083524319274655701d799f72cf952134
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-12-22 20:03:43 +0530

    Control Flow else if

--- Tech_with_Tim/Control_Flow/src/main.rs
+++ Tech_with_Tim/Control_Flow/src/main.rs
@@ -1,12 +1,26 @@
 fn main() {
     //Control Flow 
     //if..else
-
     let food = "bread";
     if food == "bread"{
         println!("That sounds boring");
     }
     else{
-        println!("Oh! that's not good")
+        println!("Oh! that's not good");
     }
+
+    //else if
+    let food = "cookie";
+    if food == "bread"{
+        println!("That sounds boring");
+    }
+    else if food == "cookie"{
+        println!("I love cookies too");
+    }
+    else{
+        println!("Oh! that's not good");
+    }
+
+
+
 }
