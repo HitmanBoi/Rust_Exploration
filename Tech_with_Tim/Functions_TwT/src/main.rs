17bd88281e942f4a5586e486a784c64d378c3d4d
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-12-24 20:02:02 +0530

    Functions Creating and naming convention

--- Tech_with_Tim/Functions_TwT/src/main.rs
+++ Tech_with_Tim/Functions_TwT/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     //Functions
+    //Naming Convention in Rust is to use snake case (function_name)
+    //Function declaration syntax
+    fn test_one(){
+        println!("Test is called!");
+    }
 }
