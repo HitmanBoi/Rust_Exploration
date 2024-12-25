2568ff0a67b6acf20b879e51445388a7da302335
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-12-25 20:34:21 +0530

    Calling before declaring , (Compiled language , order doesnt affect)

--- Tech_with_Tim/Functions_TwT/src/main.rs
+++ Tech_with_Tim/Functions_TwT/src/main.rs
@@ -1,8 +1,3 @@
-//Functions with parameters
-fn add_numbers(x:i32 , y:i32){   //We need to explicitly define the type of parameters and multiple parameters are seperated by a comma
-    println!("The sum is : {}",x+y) //It will not return if we put a semicolon(;) here , Expressions vs statements
-}
-
 fn main() {
     //Functions
     //Naming Convention in Rust is to use snake case (function_name)
@@ -15,8 +10,13 @@ fn main() {
     add_numbers(20,30);
 
 
-    
 
+
+}
+
+//Functions with parameters
+fn add_numbers(x:i32 , y:i32){   //We need to explicitly define the type of parameters and multiple parameters are seperated by a comma
+    println!("The sum is : {}",x+y) //It will not return if we put a semicolon(;) here , Expressions vs statements
 }
 
 
