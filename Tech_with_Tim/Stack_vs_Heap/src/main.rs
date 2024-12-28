7488ca237dab18be234161280e6625c699399a2b
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-12-28 20:37:03 +0530

    Stack behaviour

--- Tech_with_Tim/Stack_vs_Heap/src/main.rs
+++ Tech_with_Tim/Stack_vs_Heap/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     //Stack and Heap
+
+    //The data having fixed size in memory is stored inside stack in RAM and it follows general (LIFO / FILO) operation
+
+    //example
+    let x = 2;
+    let y = x;
+    println!("y i.e. {} is pooped",y);
+    println!("x i.e. {} is pooped",x);
+
 }
