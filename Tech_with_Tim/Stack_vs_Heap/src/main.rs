29fb7915253f75fa0c6219b53c888e69c2aec628
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-12-29 20:30:27 +0530

    Heap behaviour

--- Tech_with_Tim/Stack_vs_Heap/src/main.rs
+++ Tech_with_Tim/Stack_vs_Heap/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     //Stack and Heap
 
+    //Stack
     //The data having fixed size in memory is stored inside stack in RAM and it follows general (LIFO / FILO) operation
 
     //example
@@ -8,5 +9,17 @@ fn main() {
     let y = x;
     println!("y i.e. {} is pooped",y);
     println!("x i.e. {} is pooped",x);
+
+    //Heap
+    //The data having/requiring dynamic size in memory is stored in heap in RAM and this heap is different from that data type.
+    //The adderess of data as in heap is stored in the stack as pointer referance.
+
+    //example
+    let x = 2;
+    let String1 = String::from("Hello"); //Dynamic String
+
+    println!("String1 is popped from stack");
+    println!("{} is removed from Heap since owner (i.e. String1) is popped",String1);
+    println!("x i.e. {} is popped from stack",x);
 
 }
