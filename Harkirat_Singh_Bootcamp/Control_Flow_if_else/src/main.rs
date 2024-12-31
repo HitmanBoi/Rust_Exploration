f3e02524815b771d5094bfc82dacc7b300f87b07
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-12-31 20:04:39 +0530

    Harkirat If..else basic

--- Harkirat_Singh_Bootcamp/Control_Flow_if_else/src/main.rs
+++ Harkirat_Singh_Bootcamp/Control_Flow_if_else/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     //if..else
-    
+    let is_even = true ;
+    if is_even{
+        println!("The n is even");
+    }
+    else if !is_even {
+        println!("The n is not even");
+    }
 }
